ce83152918b3837af9f737792c74502558a732fa
hecmao1991 <hecmao1991@gmail.com>
2017-05-28 21:55:27 -0500

    agrega autor

--- recarga/src/Control/ControlCliente.java
+++ recarga/src/Control/ControlCliente.java
@@ -15,7 +15,7 @@ import org.omg.CORBA.OBJECT_NOT_EXIST;
 
 
 public class ControlCliente {
-
+    //prueba ingreso
     Persistencia p = new Persistencia();
 
     public boolean insertarCliente(String cedula, String direccion) {
